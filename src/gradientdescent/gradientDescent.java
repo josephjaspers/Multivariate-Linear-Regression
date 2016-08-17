e545c42b6f5bed8cff152d46c39940435c11e455
josephjaspers <jj804785@wcupa.edu>
2016-08-16 22:12:45 -0400

    updated - added Y intercept
    //needs optimization (mean normalization / feature scaling)

--- src/gradientdescent/gradientDescent.java
+++ src/gradientdescent/gradientDescent.java
@@ -13,12 +13,13 @@ import java.util.ArrayList;
  */
 public class gradientDescent {
 
+    int minimumIterations;  
     double tolerance = 0.000000001;
     boolean converged = false;
     //Convergence check not yet supported 
-    double learningRate = 0.0003;
+    double learningRate = 0.003;
 
-    double[] constantArray;
+    double[] constants;
     ArrayList<double[]> trainingData;  // the last double element is the Y of each Theta 
     String[] featureNames;
     int numbFeats;
@@ -27,8 +28,8 @@ public class gradientDescent {
 
     public gradientDescent(String[] featureNames) {
         this.featureNames = featureNames;
-        numbFeats = featureNames.length;
-        constantArray = new double[featureNames.length];
+        numbFeats = featureNames.length + 1; //y(last feature is the y intercept)
+        constants = new double[featureNames.length + 1]; //last feature y intercept
         trainingData = new ArrayList<>();
     }
 
@@ -45,27 +46,31 @@ public class gradientDescent {
     //http://cs229.stanford.edu/notes/cs229-notes1.pdf (The algorithm is on pg. 5)
     public double costFunction(int j, int i) { //cost function for single update 
         int m = trainingData.size();
-        double constant = constantArray[j];
+        double constant = constants[j];
 
         double sigma = 0;
         sigma += (y(i) - HypothesisTheta(i)) * x(i, j);
         sigma *= learningRate;
         constant += sigma;
 
-        constantArray[j] = constant;
+        constants[j] = constant;
         return constant;
     }
 
     public double HypothesisTheta(int index) {
         double total = 0;
         for (int i = 0; i < numbFeats; ++i) {
-            total += trainingData.get(index)[i] * constantArray[i];
+            if (i == numbFeats - 1) {
+                total += constants[i]; //Y intercept
+            } else {
+                total += trainingData.get(index)[i] * constants[i];
+            }
         }
         return total;
     }
 
     public double y(int index) {
-        return trainingData.get(index)[numbFeats]; //Get the last element in trainingData which is the learningData
+        return trainingData.get(index)[numbFeats - 1]; //Get the last element in trainingData which is the learningData
     }
 
     public double x(int index, int jIndex) {
@@ -73,15 +78,15 @@ public class gradientDescent {
     }
 
     public void addLearningData(double[] data) {
-        if (data.length != numbFeats + 1) {
-            throw new IllegalArgumentException("Must have all features + solution");
+        if (data.length != numbFeats) {
+            throw new IllegalArgumentException("Must have all features + solution - curr sz: " + data.length);
         }
         cacheUpdated = true;
         trainingData.add(data);
     }
 
     public double evaluate(double[] data) {
-        if (data.length != numbFeats) {
+        if (data.length != numbFeats - 1) {//Excludes y intercept
             throw new IllegalArgumentException("invalid number of features");
         }
 
@@ -91,13 +96,13 @@ public class gradientDescent {
 
         double total = 0;
         for (int i = 0; i < data.length; ++i) {
-            total += data[i] * constantArray[i];
+            total += data[i] * constants[i];
         }
         return total;
     }
 
     public double[] getConstants() {
-        return constantArray;
+        return constants;
     }
 
     public static void main(String[] args) {
@@ -111,7 +116,7 @@ public class gradientDescent {
         double f3;
         double ans;
 
-        for (int i = 0; i < 4000; i++) {
+        for (int i = 0; i < 1000; i++) {
             f1 = Math.random() * 10 + 10;
             f2 = Math.random() * 10 + 10;
             f3 = Math.random() * 10 + 10;
